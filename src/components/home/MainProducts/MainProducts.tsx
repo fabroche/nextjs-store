0ae40ca646c28ff728f521c7e44a6a316b6986ac
fabroche <kraxusmmo@gmail.com>
2025-09-23 22:24:47 +0100

    feat: connecting to shopify cms store

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,24 @@
-export function MainProducts() {
+async function getProducts() {
+    const options = {
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY ?? '',
+        }),
+    }
+
+    const response = await fetch(
+        `${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-07/products.json`,
+        options
+    )
+
+    const data = await response.json()
+
+    return data;
+}
+
+export async function MainProducts() {
+
+    const products = await getProducts();
+    console.log(products)
     return (
         <section>
             <h1>MainProducts</h1>
